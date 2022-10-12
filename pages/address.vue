dd455b5c19bad90b6d96b7820ad86bdf3c33594f
Maxime Coriton <maxime.coriton@gmail.com>
2022-10-12 17:37:10 +0200

    api en cours, location is good

---
+++ pages/address.vue
@@ -0,0 +1,50 @@
+<template>
+    hello
+    <div class="container">
+        <h1 class="status"></h1>
+        <button class="find-state">Find my state</button>
+    </div>
+</template>
+
+<script >
+export default {
+
+    /* https://stackoverflow.com/questions/56180458/referenceerror-document-is-not-defined-in-svelte-3 */
+    mounted() {
+
+        const findMyState = () => {
+            const status = document.querySelector('.status')
+
+            const success = (position) => {
+                console.log(position)
+                const latitude = position.coords.latitude
+                const longitude = position.coords.longitude
+                /* console.log(latitude + ' ' +  longitude) */
+
+                const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+
+                /* Promesse */
+                fetch(geoApiUrl)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        /* Ecris là où il y a la class status la localité */
+                        status.textContent = data.locality
+                    })
+            }
+
+            const error = () => {
+                status.textContent = 'Unable to retrieve your location'
+            }
+
+            navigator.geolocation.getCurrentPosition(success, error)
+        }
+
+        document.querySelector('.find-state').addEventListener('click', findMyState)
+    }
+}
+
+
+</script>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+
+
+<template>
+    <header>
+        <nuxt-link to="/address">Address</nuxt-link>
+    </header>
+      <input type="text" placeholder="login, id, node_id, url" id="in" v-model="text">
+      <button type="button" v-on:click="getValue">Submit</button>
+      <p>organization : {{ organization }}</p>
+      <p>Text : {{ text }}</p>
+      <!-- <p>organization.text : {{ organization.text }}</p> -->
+  
+  
+      <p>organization.login : {{ organization.login }}</p>
+      <p>organization.description : {{ organization.description }}</p>
+  </template>
+  
+  <script >
+  
+  export default defineComponent({
+    async setup() {
+  
+      const [{ data: organization }, { data: repos }] = await Promise.all([
+        useFetch(`https://api.github.com/orgs/nuxt`),
+        useFetch(`https://api.github.com/orgs/nuxt/repos`)
+      ])
+  
+      return {
+        organization,
+        repos
+      }
+    },
+    methods: {
+      getValue() {
+        /* Essayer avec {} pour la variable et $ pour l'appeler */
+        // Sélectionner l'élément input et récupérer sa valeur
+        var { text } = document.getElementById("in").value;
+        // Afficher la valeur
+        console.log(text)
+      }
+    }
+  })
+  
+  </script>
